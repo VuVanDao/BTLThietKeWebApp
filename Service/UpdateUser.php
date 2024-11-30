2ad5c5a8245294304d58879ec46dac564c514771
Van Dao <dao28901@gmail.com>
2024-11-30 19:43:38 +0700

    first commit

---
+++ Service/UpdateUser.php
@@ -0,0 +1,32 @@
+<?php
+include "connectService.php";
+$id=$_GET['id'];
+$tenHo=$_GET["Ho"];
+$tenGoi=$_GET["Ten"];
+$email = filter_var($_GET['Email'],FILTER_SANITIZE_EMAIL);
+$NgaySinh=$_GET['birth-day'];
+$ThangSinh=$_GET['birth-month'];
+$NamSinh=$_GET['birth-year'];
+$GioiTinh = $_GET['gender'];
+$NoiSong = $_GET['NoiSong'];
+$MaNhanVien=$_GET['MaNhanVien'];
+$sql2 = "SELECT * FROM yahoo where Email = '$email'";
+$result2 = mysqLi_query($conn,$sql2);
+if(
+    $result2->num_rows <= 0 &&
+   filter_var($_GET['Ho'],FILTER_SANITIZE_STRING) &&
+   filter_var($_GET['Ten'],FILTER_SANITIZE_STRING) &&
+   filter_var($_GET['Email'],FILTER_VALIDATE_EMAIL) &&
+   filter_var($_GET['MaNhanVien'],FILTER_VALIDATE_INT)){
+$sql = " UPDATE `yahoo` 
+         SET `Ho`='$tenHo',`Ten`='$tenGoi',`NgaySinh`='$NgaySinh',`ThangSinh`='$ThangSinh'
+         ,`NamSinh`='$NamSinh',`GioiTinh`='$GioiTinh',`NoiSong`='$NoiSong',`MaNhanVien`='$MaNhanVien'
+         ,`Email`='$email'
+         WHERE id = $id"; 
+mysqLi_query($conn,$sql);
+header("location:../InfoTable.php");
+}else{
+header("location:../InfoTable.php");
+    
+}
+?>
